b37ac5db1c148e45d76e161064b80a7398aa73b6
up202006020 <up202006020@up.pt>
2024-11-26 01:17:58 +0000

    Minor fixes

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -6,16 +6,16 @@
 
         <p><strong>Event ID:</strong> {{ $report->event_id }}</p>
         <p><strong>Event Name:</strong> {{ $report->event->name }}</p>
-        <p><strong>User ID:</strong> {{ $report->description }}</p>
+        <p><strong>User ID:</strong> {{ $report->user_id }}</p>
         <p><strong>User Name:</strong> {{ $report->user->name }}</p>
         <p><strong>Reason:</strong> {{ $report->reason }}</p>
         <p><strong>Status:</strong> {{ $report->r_status}}</p>
         <p><strong>Created at:</strong> {{ $report->created_at }}</p>
         <p><strong>Updated at:</strong> {{ $report->updated_at }}</p>
+        @if (Auth::user()->isAdmin())
         <a href="{{ route('events.edit', $report->event_id) }}" class="btn btn-warning">Edit Event</a>
-        
+        @endif
         <a href="{{ route('updateReportForm', $report->report_id) }}" class="btn btn-warning">Edit Report</a>
-
         <form action="{{ route('deleteReport', $report->report_id) }}" method="POST" class="d-inline">
             @csrf
             @method('DELETE')
