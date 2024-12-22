d2bd9839cc3bb9f17ff529979307b915b8f3c4c9
up202006020 <up202006020@up.pt>
2024-12-22 14:44:22 +0000

    Fixed profile photo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 
 class ProfileController extends Controller
